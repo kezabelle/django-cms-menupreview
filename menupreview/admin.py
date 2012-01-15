63a58e64468f217e064585370644ce33bb21fa11
Keryn Knight <keryn@kerynknight.com>
2012-01-15 21:23:48 +0000

    splurging out something that might allow for the other menu tags being previewed. needs testing further.

--- menupreview/admin.py
+++ menupreview/admin.py
@@ -5,8 +5,11 @@ from django.utils.translation import ugettext_lazy as _
 from django import forms
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from menus.models import CacheKey
+from cms.models.pagemodel import Page
 from menupreview.models import MenuPreview
-from menupreview.forms import MenuPreviewForm
+from menupreview.forms import (MenuPreviewForm, SubmenuPreviewForm,
+    MenuBelowIdPreviewForm)
 
 class FakeModelAdmin(object):
     fields = None
@@ -46,32 +49,82 @@ class FakeModelAdmin(object):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
             return update_wrapper(wrapper, view)
         return patterns('',
+            url(regex=r'^cache_keys/$',
+                view=self.cache_keys,
+                name='%s_%s_cache_keys' % (self.model._meta.app_label,
+                    self.model._meta.module_name)
+            ),
             url(regex=r'^$',
-                view=self.changelist_view,
+                view=self.index,
                 name='%s_%s_changelist' % (self.model._meta.app_label,
                     self.model._meta.module_name)
-            )
+            ),
+            url(regex=r'^show_menu/$',
+                view=self.show_menu,
+                name='%s_%s_menu' % (self.model._meta.app_label,
+                    self.model._meta.module_name)
+            ),
+            url(regex=r'^show_sub_menu/$',
+                view=self.show_sub_menu,
+                name='%s_%s_sub_menu' % (self.model._meta.app_label,
+                    self.model._meta.module_name)
+            ),
+            url(regex=r'^show_menu_below_id/$',
+                view=self.show_menu_below_id,
+                name='%s_%s_menu_below_id' % (self.model._meta.app_label,
+                    self.model._meta.module_name)
+            ),
+
+
         )
     urls = property(urls)
 
-    def changelist_view(self, request, template_name='admin/menupreview/menupreview.html'):
-        form = MenuPreviewForm(request, data=request.GET or None, files=None)
+    def index(self, request):
+        return render_to_response('admin/menupreview/index.html',
+            {'app_label': self.model._meta.app_label, 'opts': self.opts,
+            'using_reverse_ids': Page.objects.filter(reverse_id__isnull=False).exists(),
+            'title': _('Menu preview administration')},
+            context_instance=RequestContext(request))
+ 
+    def cache_keys(self, request):
+       return render_to_response('admin/menupreview/cache_keys.html',
+            {'app_label': self.model._meta.app_label, 'opts': self.opts,
+            'title': _('Cache keys'), 'object_list': CacheKey.objects.all()},
+            context_instance=RequestContext(request))
+ 
+    def all_menus(self, request, template, subtemplate, form_class, title):
+        form = form_class(request, data=request.GET or None, files=None)
         if request.is_ajax():
-            template_name = '_ajax.'.join(template_name.rsplit('.', 1))
+            template = subtemplate #'_ajax.'.join(subtemplate.rsplit('.', 1))
 
         # oh my word. turns out request.path, at least, is mutable (but POST/GET
         # aren't?! Even so, we'll take a copy and modify it.
         if form.is_valid():
             request = copy(request)
             request.path = form.cleaned_data.get('active_page', request.path)
-
         ctx = {
             'form': form,
             'app_label': self.model._meta.app_label,
             'opts': self.opts,
-            'title': _('Menu preview'),
+            'title': _(title),
+            'subtemplate': subtemplate,
         }
-        return render_to_response(template_name, ctx,
+        return render_to_response(template, ctx,
             context_instance=RequestContext(request))
+  
+    def show_menu(self, request):
+        return self.all_menus(request, 'admin/menupreview/form.html',
+            'admin/menupreview/menupreview_ajax.html', MenuPreviewForm,
+            'Full menu preview')
+
+    def show_sub_menu(self, request):
+        return self.all_menus(request, 'admin/menupreview/form.html',
+            'admin/menupreview/submenupreview_ajax.html', SubmenuPreviewForm,
+            'Sub menu preview')
+
+    def show_menu_below_id(self, request):
+        return self.all_menus(request, 'admin/menupreview/form.html',
+            'admin/menupreview/menubelowidpreview_ajax.html', MenuBelowIdPreviewForm,
+            'Menu below ID preview')
 admin.site.register(MenuPreview, FakeModelAdmin)
 
--- menupreview/forms.py
+++ menupreview/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
 from menus.menu_pool import menu_pool
+from cms.models.pagemodel import Page
 
 level_opts = [(x, x) for x in xrange(0, 21)]
 
@@ -22,3 +23,33 @@ class MenuPreviewForm(forms.Form):
         if cd.get('start_level', 0) > cd.get('end_level', 0):
             raise forms.ValidationError('boo hiss')
         return cd
+
+
+class SubmenuPreviewForm(forms.Form):
+    end_level = forms.TypedChoiceField(label=_('Ending at level'), initial=20, choices=level_opts, coerce=int)
+    active_page = forms.ChoiceField(label=_('Selected page'), initial='/')
+
+    def __init__(self, request, *args, **kwargs):
+        super(SubmenuPreviewForm, self).__init__(*args, **kwargs)
+        self.fields['active_page'].choices = [(n.get_absolute_url(), mark_safe(u'%s%s' % ('&nbsp;&nbsp;'* n.level, n.title)))
+            for n in menu_pool.get_nodes(request)]
+
+
+class MenuBelowIdPreviewForm(forms.Form):
+    start_level = forms.TypedChoiceField(label=_('Starting from level'), initial=0, choices=level_opts, coerce=int)
+    end_level = forms.TypedChoiceField(label=_('Ending at level'), initial=20, choices=level_opts, coerce=int)
+    extra_inactive = forms.TypedChoiceField(label=_('Extra inactive'), initial=20, choices=level_opts, coerce=int)
+    extra_active = forms.TypedChoiceField(label=_('Extra active'), initial=20, choices=level_opts, coerce=int)
+    active_page = forms.ChoiceField(label=_('Selected page'), help_text=_('Only pages with a reverse ID are available'))
+
+    def __init__(self, request, *args, **kwargs):
+        super(MenuBelowIdPreviewForm, self).__init__(*args, **kwargs)
+        self.fields['active_page'].choices = [(p.reverse_id, p.get_menu_title())
+            for p in Page.objects.filter(reverse_id__isnull=False)]
+
+    def clean(self):
+        cd = self.cleaned_data
+        if cd.get('start_level', 0) > cd.get('end_level', 0):
+            raise forms.ValidationError('boo hiss')
+        return cd
+
